d36f1e6b836a2e23b5daf0e8363b69ddf054c782
Nam Nguyen <nam.nguyen@kongstudios.net>
2022-10-24 09:25:00 +0700

    add corsConfigurer

--- src/main/java/com/jewel/JewelApplication.java
+++ src/main/java/com/jewel/JewelApplication.java
@@ -5,6 +5,10 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletContext;
 import java.io.FileInputStream;
@@ -13,6 +17,16 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class JewelApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer () {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(JewelApplication.class, args);
 	}
